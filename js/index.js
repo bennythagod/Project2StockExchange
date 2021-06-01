bd5d75860719b8d43a4c304723c1a0951f3e5131
Bennythagode <baruchbenb@gmail.com>
2021-06-01 12:25:20 +0300

    Milestone 1

---
+++ js/index.js
@@ -0,0 +1,76 @@
+//https://financialmodelingprep.com/developer/docs/companies-key-stats-free-api/#Javascript
+function Search() {
+
+
+    document.querySelector("#tblSearch").innerHTML = `
+<tr>
+<td>
+Loading...
+</td>
+</tr>
+    `;
+
+    getRequest(
+        `https://stock-exchange-dot-full-stack-course-services.ew.r.appspot.com/api/v3/search?query=${search.value}&limit=10&exchange=NASDAQ`,
+        (responseText) => {
+            let list = JSON.parse(responseText);
+
+
+            let rowsStr = "";
+
+
+            for (const item of list) {
+                rowsStr += `
+<tr>
+    <td>
+        <div class="widget-26-job-title">
+            <a href="/company.html?symbol=${item.symbol}" target="_blank">
+                ${item.name} (${item.symbol})
+            </a>
+        </div>
+    </td>
+</tr>
+    `;
+            }
+            document.querySelector("#tblSearch").innerHTML = rowsStr;
+
+        });
+}
+
+
+
+
+
+
+
+
+
+
+function getRequest(url, success) {
+    var req = false;
+    try {
+        req = new XMLHttpRequest();
+    } catch (e) {
+        try {
+            req = new ActiveXObject("Msxml2.XMLHTTP");
+        } catch (e) {
+            try {
+                req = new ActiveXObject("Microsoft.XMLHTTP");
+            } catch (e) {
+                return false;
+            }
+        }
+    }
+    if (!req) return false;
+    if (typeof success != 'function') success = function() {};
+    req.onreadystatechange = function() {
+        if (req.readyState == 4) {
+            if (req.status === 200) {
+                success(req.responseText)
+            }
+        }
+    }
+    req.open("GET", url, true);
+    req.send(null);
+    return req;
+}
